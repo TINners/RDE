ab80bcbb50b827c652835f52702038123f6035bc
Adrian <a.obrebski93@gmail.com>
2015-06-08 02:17:38 +0200

    Autoryzacja bez back info i hasha

--- rde/core/views/auth.py
+++ rde/core/views/auth.py
@@ -4,6 +4,8 @@ Authorization view - supports logging users in, out and changing their passwords
 
 from django.views.generic import View
 from django.shortcuts import render
+import xml.etree.ElementTree as ET
+
 
 class Auth(View):
     def get(self, request):
@@ -20,16 +22,55 @@ class Auth(View):
         them to the listing page.
         Otherwise render the login page with the errors highlighted.
         """
-
-        # TODO: implement
-        return render(request, "login.html")
-
+        # to improve
+        fileName = 'users.xml'
+        l = request.POST['login']
+        p = request.POST['password']
+        tree = ET.parse(fileName);
+        root = tree.getroot();
+        index = getUserIndexByLogin(l, root)
+        
+        if index == -1:
+            # write niepoprawny login
+            return render(request, "login.html")          # blad autoryzacji, niepoprawny login
+            
+        if root[index][1].text != None:                       # Jest password w bazie
+            if(root[index][1].text != p):
+                # write niepoprawne haslo                 # password nie zgadza sie
+                return render(request, "login.html")      # blad autoryzacji
+                
+        elif p != None and p != '':                       # podal haslo
+            updatePassword(index, p, tree, fileName)      # update hasla
+       
+        request.session['login'] = l
+        return render(request, "listing.html")
+        
+            
     def delete(self, request):
         """
         On DELETE, remove user's authorization from their session
         and render the login page with a "logged out" message.
         """
-
-        # TODO: implement
+        
+        # to improve. Need to write "logged out"
+        request.session['login'] = None
         return render(request, "login.html")
-
+        
+        
+    def getUserIndexByLogin(l, root):
+        i = 0
+        for r in root:
+            if(r[0].text == l):
+                return i
+            i = i + 1
+        
+        return -1
+        
+        
+    def updatePassword(index, password, tree, fileName):
+        root = tree.getroot()
+        root[index][1].text = password
+        file = open(fileName, mode='w')
+        tree.write(file, encoding="unicode")
+        file.close()
+        return
